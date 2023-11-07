4625d5832389ef85447862afca3e1902935ef21d
Abrahm <abrahmrollins@gmail.com>
2023-11-06 17:47:59 -0800

    Final Styling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'brown', 'gray', 'black', 'white', 'maroon', 'navy', 'teal', 'olive'];
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'brown', 'gray', 'black', 'white', 'maroon', 'navy', 'teal', 'olive', 'aqua', 'beige', 'coral', 'crimson', 'cyan', 'fuchsia', 'gold', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'magenta', 'mint', 'mustard', 'peach', 'peru', 'plum', 'salmon', 'sienna', 'silver', 'skyblue', 'tan', 'tomato', 'turquoise', 'violet', 'wheat', 'yellowgreen', 'darkred', 'darkorange', 'darkgreen', 'darkblue', 'darkviolet', 'darkgray', 'lightgray'];
 
 const changeColor = document.getElementById('changeColor');
 const currentColor = document.getElementById('currentColor');
