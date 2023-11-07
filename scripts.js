b20a99a5e2d62a1e9f213352aa9803d8b24d2922
Abrahm <abrahmrollins@gmail.com>
2023-11-06 17:24:44 -0800

    Added feature to display the name of the current color on the screen

--- scripts.js
+++ scripts.js
@@ -2,9 +2,10 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'brown', 'gray', 'black', 'white', 'maroon', 'navy', 'teal', 'olive'];
 
 const changeColor = document.getElementById('changeColor');
+const currentColor = document.getElementById('currentColor');
 
 changeColor.addEventListener('click', () => {
     const randomColor = Math.floor(Math.random() * colors.length);
     document.body.style.backgroundColor = colors[randomColor];
+    currentColor.textContent = colors[randomColor];
 });
-
